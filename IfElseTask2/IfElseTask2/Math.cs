25472bb7924593a8cff95f3eab77dc2cf90fd440
Nickolas Nordskog <nickolas.nordskog@gmail.com>
2024-12-06 02:08:47 +0100

    initial

---
+++ IfElseTask2/IfElseTask2/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseTask2
+{
+    internal class Math
+    {
+        public static int Main()
+        {
+            int number = 6;
+            int number2= 9;
+
+            if (number == number2)
+            {
+                Console.WriteLine(number * number2);
+                return (number * number2);
+            }
+            else
+            {
+                Console.WriteLine(number + number2);
+                return (number + number2);
+            }
+        }
+        
+    }
+}
---
+++ IfElseTask2/IfElseTask2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Oppgave 2: 
+// Lag en metode som tar imot to tall og returnerer summen av tallene dersom de er ulike, 
+// og returnerer tallene multiplisert med hverandre dersom de er like
+IfElseTask2.Math.Main();
