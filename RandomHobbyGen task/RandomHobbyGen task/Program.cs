69f296003c386598a6efe05213b5af1b8868eb7d
Nickolas Nordskog <nickolas.nordskog@gmail.com>
2024-12-11 03:07:00 +0100

    Initial commit

---
+++ RandomHobbyGen task/RandomHobbyGen task/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Who would like a hobby?");
+
+string name = Console.ReadLine();
+
+string[] hobbies =
+{
+    "playing hopskotch with illegal cats",
+    "stealing with raccoons",
+    "farting on people with skunks",
+    "biting people with possums",
+    "noodling with ferrets",
+    "bEing imposter raccoons with red pandas",
+    "laughing with foxes"
+};
+
+Random rand = new Random();
+var randomHobby = rand.Next(0, hobbies.Length);
+Console.WriteLine(name + " is " + hobbies[randomHobby]);
